06fcfc8099892ab402a104ac00edb9f7a33a9784
hmtechnology <hmtechnology@users.noreply.github.com>
2023-10-11 13:45:37 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,15 +11,25 @@ date_default_timezone_set('Europe/Rome');
 // Define the opening and closing hours for each day
 $store_hours = array(
     'monday'    => array(
-        array('open' => '09:00', 'close' => '12:00'), // Two opening and closing times on Monday
-        array('open' => '15:00', 'close' => '18:00')  // Two opening and closing times on Monday
+        array('open' => '09:00', 'close' => '12:00'),
+        array('open' => '15:00', 'close' => '18:00')
     ),
-    'tuesday'   => array('open' => '09:00', 'close' => '18:00'),
-    'wednesday' => array('open' => '09:00', 'close' => '18:00'),
-    'thursday'  => array('open' => '09:00', 'close' => '18:00'),
-    'friday'    => array('open' => '09:00', 'close' => '18:00'),
-    'saturday'  => array('open' => '10:00', 'close' => '15:00'),
-    'sunday'    => array('open' => '00:00', 'close' => '00:00'), // Closed on Sunday
+    'tuesday'   => array(
+        array('open' => '09:00', 'close' => '18:00')
+    ),
+    'wednesday' => array(
+        array('open' => '09:00', 'close' => '13:00')
+    ),
+    'thursday'  => array(
+        array('open' => '09:00', 'close' => '18:00')
+    ),
+    'friday'    => array(
+        array('open' => '09:00', 'close' => '18:00')
+    ),
+    'saturday'  => array(
+        array('open' => '10:00', 'close' => '15:00')
+    ),
+    'sunday'    => array() // Closed on Sunday
 );
 
 // Function to check if the store is open
